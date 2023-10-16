b7e0091aa455c5fd01538dc756d0f10ef7f0a83e
Tanatorn5 <146711234+Tanatorn5@users.noreply.github.com>
2023-10-16 14:57:16 +0700

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,23 @@
+
+public class Test {
+	 public static void main(String[] args) {
+		 ArrayList<Character> ar = new ArrayList<>();
+		 ar.add(0,'A');
+		 ar.add(0,'B');
+		 ar.get(1);
+		 System.out.println("Get : "+ar.get(1));
+		 System.out.println("Error");
+		 //ar.set(2,'C');
+		 ar.add(2,'C');
+		 System.out.println("Error");
+		 //ar.add(4,'D');
+		 System.out.println("Remove: "+ar.remove(1));
+		 ar.remove(1);
+		 ar.add(1,'D');
+		 ar.add(1,'E');
+		 //ar.get(4);
+		 System.out.println("Error");
+		 System.out.println("set: "+ar.set(2,'6'));
+		 System.out.println("get: "+ar.get(2));
+	 }
+}
